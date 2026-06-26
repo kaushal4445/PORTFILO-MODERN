16062eb9b2295a6c817348d2262217853590a7db
kaushal4445 <mehrakaushal54@gmail.com>
2026-06-26 23:13:23 +0530

    Initial Portfolio

--- beautiful-portfilo/uuy/src/Components/sections/Home.jsx
+++ beautiful-portfilo/uuy/src/Components/sections/Home.jsx
@@ -36,8 +36,7 @@ export const Home = () => {
             </a>
               {/* 👇 Download Resume Button */}
   <a
-    href=""
-  
+    href="https://drive.google.com/file/d/13x-JnWuqzBewT5fB7NAgbUVX2Gd5O7Gt/view?usp=drive_link"
     className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
   >
     Download Resume
